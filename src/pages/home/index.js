e485af93d5f6073174afd8e8a6d325440b2923f0
Tanay <tanaydwivedi456@gmail.com>
2022-02-15 17:25:57 +0530

    feat:styled user card

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,26 @@
 import React from "react";
 import image from "./mp.jpg";
+import { FaTimes, FaCheck } from "react-icons/fa";
 import "./home.css";
 
 const HomePage = () => {
+  /*const handleCase=()=>{
+
+  }*/ //to handle the case when user swipe right or left
 
   return (
-
-    <div className="card">
-      <div className="container">
-        <img src={image} alt="user-image" width="100%"/>
-        <h2>Username</h2> 
-        <p>Tags</p> 
+    <div>
+      <div className="card">
+        <div className="container">
+          <img src={image} alt="user-image" width="60%" className="centerImg" />
+          <h1>Username</h1>
+          <p>Tags</p>
+        </div>
       </div>
-      
+      <FaTimes className=""/>
+      <FaCheck className=""/>
     </div>
-    
   );
 };
 
 export default HomePage;
-
-
-
-
-
-
-
-
