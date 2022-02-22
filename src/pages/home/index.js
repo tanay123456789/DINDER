b1ae2a793ffaae2e9c72aec35a71419516e43982
Tanay <tanaydwivedi456@gmail.com>
2022-02-22 22:52:36 +0530

    added swipe features

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,81 @@
-import React from "react";
-import image from "./mp.jpg";
-import { FaTimes, FaCheck } from "react-icons/fa";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+//import { FaTimes, FaCheck } from "react-icons/fa";
+
+
+
+
+import Card from "./Card";
+
+
 import "./home.css";
 
-/*const [userName,setUserName]=useState(null);
-
-const [isSwipeRight,setSwipeRight]=useState(false);
-
-const RejectedList=new Array.fill("empty",0);
 
 
-const AcceptedList=new Array.fill("empty",0);
-*/
+
+
+
+
+/*const handleCase=()=>{
+
+}*/ //to handle the case when user swipe right or left
+
+/*<img src={image} alt="user-image" width="60%" className="centerImg" /> 
+        <h1>Username</h1>
+        <p>Tags</p>*/
 
 const HomePage = () => {
-  /*const handleCase=()=>{
 
-  }*/ //to handle the case when user swipe right or left
+  const [userName, setUserName] = useState(null);
 
-  return (
-    <div>
-      <div className="card">
-        <div className="container">
-          <img src={image} alt="user-image" width="60%" className="centerImg" />
-          <h1>Username</h1>
-          <p>Tags</p>
-        </div>
-      </div>
-      <FaTimes className="timeIcon"/>
-      <FaCheck className="checkIcon"/>
-    </div>
+
+  const [isSwipeRight, setSwipeRight] = useState(false);
+
+  
+  return(
+    <Card/>
+
+
+
+
+
+
   );
+  
+
+
+
+
+  /*return(
+    <div>
+      <Card/>
+      <FaTimes className="timeIcon" />
+      <FaCheck className="checkIcon" />
+
+
+
+    </div>
+
+    
+
+  );*/
+
+
+
+ 
+
+      
+
+           
+
+          
+          
+          
+        
+      
+    
+  
 };
 
 export default HomePage;
-
-
-
