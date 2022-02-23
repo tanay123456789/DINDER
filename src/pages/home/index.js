77469794ad15778177437e9780f111e82a1e9508
Tanay <tanaydwivedi456@gmail.com>
2022-02-23 20:25:22 +0530

    merged two components in homepage into one

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,81 +1,134 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useState, useMemo, useRef } from "react";
+import h from "./styles.modules.css";
+// import TinderCard from '../react-tinder-card/index'
+import TinderCard from "react-tinder-card";
 
-//import { FaTimes, FaCheck } from "react-icons/fa";
+const acceptedList=[];
+const rejectedList=[];
 
+const db = [
+  {
+    name: "Richard Hendricks",
+    url: "./img/richard.jpg"
+  },
+  {
+    name: "Erlich Bachman",
+    url: "./img/erlich.jpg"
+  },
+  {
+    name: "Monica Hall",
+    url: "./img/monica.jpg"
+  },
+  {
+    name: "Jared Dunn",
+    url: "./img/jared.jpg"
+  },
+  {
+    name: "Dinesh Chugtai",
+    url: "./img/dinesh.jpg"
+  }
+];
 
+const HomePage= ()=> {
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
 
-
-import Card from "./Card";
-
-
-import "./home.css";
-
-
-
-
-
-
-
-/*const handleCase=()=>{
-
-}*/ //to handle the case when user swipe right or left
-
-/*<img src={image} alt="user-image" width="60%" className="centerImg" /> 
-        <h1>Username</h1>
-        <p>Tags</p>*/
-
-const HomePage = () => {
-
-  const [userName, setUserName] = useState(null);
-
-
-  const [isSwipeRight, setSwipeRight] = useState(false);
-
-  
-  return(
-    <Card/>
-
-
-
-
-
-
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        // eslint-disable-next-line no-unused-vars
+        .map((i) => React.createRef()),
+    []
   );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
   
 
+  const canSwipe = currentIndex >= 0;
 
+  // set last direction and decrease current index
+  const swiped = (direction, character, index) => {
+    if(direction=="left"){
+      rejectedList.push(character);
+    }
+    else{
+      acceptedList.push(character);
+    }
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
 
+  };
 
-  /*return(
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  
+
+  return (
     <div>
-      <Card/>
-      <FaTimes className="timeIcon" />
-      <FaCheck className="checkIcon" />
-
-
-
+      <link
+        href='https://fonts.googleapis.com/css?family=Damion&display=swap'
+        rel='stylesheet'
+      />
+      <link
+        href='https://fonts.googleapis.com/css?family=Alatsi&display=swap'
+        rel='stylesheet'
+      />
+     
+      <div className={h.cardContainer}>
+        {db.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className={h.swipe}
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+          >
+            <div
+              style={{ backgroundImage: "url(" + character.url + ")" }}
+              className={h.card}
+            >
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+      <div className={h.buttons}>
+        <button style={{ backgroundColor: !canSwipe && "#c3c4d3" }} onClick={() => swipe("left")}>Swipe left!</button>
+      
+        <button style={{ backgroundColor: !canSwipe && "#c3c4d3" }} onClick={() => swipe("right")}>Swipe right!</button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className={h.infoText}>
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className={h.infoText}>
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
     </div>
-
-    
-
-  );*/
-
-
-
- 
-
-      
-
-           
-
-          
-          
-          
-        
-      
-    
-  
+  );
 };
 
 export default HomePage;
