5b27d15c084e590426a3163ac913e7cb5728c863
Rishu Patel <rishupatel04@gmail.com>
2022-02-27 05:02:21 +0530

    feat: ✨ add ability to chat with connected users.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import AuthProvider from "./context/authContext";
 
 // Toaster for notifications and alerts
 import { Toaster } from "react-hot-toast";
+import ChatProvider from "./context/chatContext";
 
 const App = () => {
   return (
@@ -25,7 +26,16 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path="chat" element={<ChatPage />} />
+
+            <Route
+              path="chat"
+              element={
+                <ChatProvider>
+                  <ChatPage />
+                </ChatProvider>
+              }
+            />
+
             <Route path="profile" element={<ProfilePage />} />
             <Route path="users/:username" element={<UserPage />} />
             <Route path="*" element={<NotFound />} />
