0efd078016feeb22e7b07c7148b908d40615bd09
Rishu Patel <rishupatel04@gmail.com>
2022-02-28 07:22:40 +0530

    🔨 add signout, drawer menu in navigation menu
    This adds a signout button in the navigation bar and also adds a
    hamburger menu for the mobile displays thats triggers a drawer.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import s from "./styles.module.css";
 import { BsChatDots } from "react-icons/bs";
+import { VscSignOut } from "react-icons/vsc";
 import { AuthContext } from "../../context/authContext";
+import { auth } from "../../firebase";
+import { BiMenu, BiUserCircle } from "react-icons/bi";
+import { Drawer } from "artemis-ui";
 
 const Navigation = () => {
   const { user, username } = useContext(AuthContext);
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+
+  const hideDrawer = () => {
+    setIsDrawerVisible(false);
+  };
+
+  const showDrawer = () => {
+    setIsDrawerVisible(true);
+  };
+
+  const hanldeSignOut = () => {
+    auth.signOut();
+  };
+
   return (
     <nav className={s.nav}>
       <ul>
@@ -14,19 +32,47 @@ const Navigation = () => {
             <h1>DINDER</h1>
           </Link>
         </li>
+
         <li className={s.chat}>
           <Link to="/chat">
             <BsChatDots />
             <span>Chat</span>
           </Link>
         </li>
-        <li>
+
+        <li className={s.signout} onClick={hanldeSignOut}>
+          <VscSignOut />
+          Sign out
+        </li>
+
+        <li className={s.profile}>
           <Link to="/profile" className={s.user}>
             <img src={user.photoURL} alt={user.displayName} />
             <span>{username}</span>
           </Link>
         </li>
+
+        <li onClick={showDrawer} className={s.menu}>
+          <BiMenu style={{ fontSize: "1.8rem" }} />
+        </li>
       </ul>
+
+      <Drawer
+        isVisible={isDrawerVisible}
+        onClose={hideDrawer}
+        placement="bottom"
+        height="fit-content"
+      >
+        <Link to="/profile">
+          <p className={s.drawerItem}>
+            <BiUserCircle style={{ fontSize: "1.8rem" }} />
+            Profile
+          </p>
+        </Link>
+        <p className={s.drawerItem} onClick={hanldeSignOut}>
+          <VscSignOut style={{ fontSize: "1.8rem" }} /> Sign out
+        </p>
+      </Drawer>
     </nav>
   );
 };
